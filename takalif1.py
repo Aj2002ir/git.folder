3458405e6026e67e8f9a7ef549316ba0d6346777
【﻿ﾑﾌ】 <abolfazalja@gmail.com>
2026-08-14 15:26:39 +0330

    Add files via upload

---
+++ takalif1.py
@@ -0,0 +1,57 @@
+ تکلیف عملی ابوالفضل جعفری 
+# ---------------------------
+# 1. print
+# ---------------------------
+print("hello")
+
+# ---------------------------
+# 2. متغیرها و انواع داده (int, float, bool)
+# ---------------------------
+first_name = "Abolfazl"
+last_name = "Jafari"
+age = 24            # int
+gpa = 18.5           # float
+is_student = True    # bool
+
+print(first_name)
+print(last_name)
+print(age)
+print(gpa)
+print(is_student)
+
+print("Full Name:", first_name, last_name)
+print("Age:", age)
+print("City: Tabriz")
+
+# ---------------------------
+# 3. عملگرهای منطقی (or, and, not)
+# ---------------------------
+x = True
+y = False
+
+print(x or y)
+print(x and y)
+print(not x)
+
+# ---------------------------
+# 4. عملگرهای ریاضی (+ - / * % **)
+# ---------------------------
+a = 26
+b = 6
+
+print(a + b)    # جمع
+print(a - b)    # تفریق
+print(a / b)    # تقسیم
+print(a * b)    # ضرب
+print(a % b)    # باقی‌مانده
+print(a ** b)   # توان
+
+# ---------------------------
+# 5. عملگرهای مقایسه‌ای (== != <= >= < >)
+# ---------------------------
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+print(a < b)
+print(a > b)
