895b009602d3c6179fbb09dcda5de8e0a6f84f68
【﻿ﾑﾌ】 <Abolfazalja@gmail.com>
2026-08-19 15:15:48 +0330

    add takalif3.py

---
+++ takalif3.py
@@ -0,0 +1,15 @@
+name = input(" نام ")
+family = input(" نام خانوادگی  ")
+age = int(input(" سن "))
+grade = float(input(" نمره "))
+print("نام و نام خانوادگی:", name, family)
+print("سن:", age)
+print("نمره:", grade)
+if grade >= 18:
+    print("وضعیت الف")
+elif grade >= 13:
+    print("وضعیت قابل قبول")
+elif grade >= 10:
+    print("وضعیت نیاز به تلاش بیشتر")
+else:
+    print("وضعیت رد")
